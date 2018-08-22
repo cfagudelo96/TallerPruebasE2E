d6b6b038176cfab19e7b7587a4ff774c25254007
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-08-22 13:55:42 -0500

    Nightwatch

--- nightwatchjs/tests/test.js
+++ nightwatchjs/tests/test.js
@@ -13,5 +13,24 @@ module.exports = { // adapted from: https://git.io/vodU0
       .waitForElementVisible('.aviso.alert.alert-danger', 4000)
       .assert.containsText('.aviso.alert.alert-danger', 'El correo y la contraseña que ingresaste no figuran')
       .end();
+  },
+  'Los estudiantes search teacher': function(browser) {
+    browser
+      .url('https://losestudiantes.co/')
+      .click('.botonCerrar')
+      .waitForElementVisible('.Select-control', 5000)
+      .pause(1000)
+      .click('.Select-control')
+      .setValue('.Select-input input', 'Mario Linares Vasquez')
+      .waitForElementVisible('.Select-menu-outer', 5000)
+      .assert.visible('.Select-menu-outer')
+      .end();
+  },
+  'Los estudiantes filter by subject': function(browser) {
+    browser
+      .url('https://losestudiantes.co/universidad-de-los-andes/ingenieria-de-sistemas/profesores/mario-linares-vasquez')
+      .waitForElementVisible('.statsProfesorDropdown', 5000)
+      .click('.materias input[type="checkbox"]')
+      .end();
   }
 };
